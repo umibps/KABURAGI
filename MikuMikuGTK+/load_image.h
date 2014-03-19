a68b6ba6ba6a96540f69cc03df55064de171b580
umibps <umihiko29@yahoo.co.jp>
2014-03-19 21:36:48 +0900

    ディレクトリ名を修正

--- MikuMikuGTK+/load_image.h
+++
@@ -1,8 +0,0 @@
-#ifndef _LOAD_IMAGE_H_
-#define _LOAD_IMAGE_H_
-
-#include "types.h"
-
-extern uint8* LoadImage(const char* utf8_path, int* width, int* height, int* channel);
-
-#endif	// #ifndef _LOAD_IMAGE_H_
